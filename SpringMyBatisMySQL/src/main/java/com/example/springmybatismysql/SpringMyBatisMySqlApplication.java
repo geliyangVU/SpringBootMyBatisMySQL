2290408f1277e31d17ddb088aeeefdb99f4e19a2
Geli Yang <gyang5@villanova.edu>
2022-05-28 16:08:00 -0400

    initial template commit

---
+++ SpringMyBatisMySQL/src/main/java/com/example/springmybatismysql/SpringMyBatisMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmybatismysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyBatisMySqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMyBatisMySqlApplication.class, args);
+    }
+
+}
---
+++ SpringMyBatisMySQL/src/test/java/com/example/springmybatismysql/SpringMyBatisMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmybatismysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMyBatisMySqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
