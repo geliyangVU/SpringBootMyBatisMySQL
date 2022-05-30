c2f7cfc7388b92c0e5eab01b6211a833faa37f21
Geli Yang <gyang5@villanova.edu>
2022-05-30 17:27:02 -0400

    implement getAll endpoints

--- SpringMyBatisMySQL/src/main/java/com/example/springmybatismysql/mapper/UsersMapper.java
+++ SpringMyBatisMySQL/src/main/java/com/example/springmybatismysql/mapper/UsersMapper.java
@@ -1,9 +1,15 @@
 package com.example.springmybatismysql.mapper;
 
 
+import com.example.springmybatismysql.model.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface UsersMapper {
 
+    @Select("select * from users")
+    List<User> findAll();
 }
--- SpringMyBatisMySQL/src/main/java/com/example/springmybatismysql/resource/UserResource.java
+++ SpringMyBatisMySQL/src/main/java/com/example/springmybatismysql/resource/UserResource.java
@@ -1,6 +1,7 @@
 package com.example.springmybatismysql.resource;
 
 
+import com.example.springmybatismysql.mapper.UsersMapper;
 import com.example.springmybatismysql.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +13,14 @@ import java.util.List;
 @RequestMapping("/rest/users")
 public class UserResource {
 
+    private UsersMapper usersMapper;
+
+    public UserResource(UsersMapper usersMapper){
+        this.usersMapper=usersMapper;
+    }
+
     @GetMapping("/all")
     public List<User> getAll(){
-        return null;
+        return usersMapper.findAll();
     }
 }
